4a0b4c6e71575b2cebdf206ac9fd43938842f601
davidhenshaw <davidhenshaw2014@gmail.com>
2021-06-18 12:28:57 -0500

    Connect user form to backend

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,24 +1,54 @@
 import React from 'react';
+import axios from 'axios';
+
+let baseURL = "http://localhost:4000";
 
 class SignupForm extends React.Component
 {
+
+    constructor(){
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            funds: 0
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post(baseURL + '/users', this.state)
+          .then((response) => {
+            console.log(response);
+          }, (error) => {
+            console.log(error);
+          });
+    }
+
+    handleChange = (event) => {
+        this.setState(
+                { [event.target.name]: event.target.value}
+            )
+    }
+
     render(){
-        
         return <div className="form">
             <h2>Sign Up!</h2>
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>
                     Username:
-                    <input type="text" name="username"/>
+                    <input value={this.state.username} onChange={this.handleChange} type="text" name="username"/>
                 </label>
                 <label>
                     Password:
-                    <input type="password" name="password"/>
+                    <input value={this.state.password} onChange={this.handleChange} type="password" name="password"/>
                 </label>
                 <label>
                     Funding Amount:
-                    <input type="number" name="funds"/>
+                    <input value={this.state.funds} onChange={this.handleChange} type="number" name="funds"/>
                 </label>
+                <button>Submit</button>
             </form>
         </div>
     }
