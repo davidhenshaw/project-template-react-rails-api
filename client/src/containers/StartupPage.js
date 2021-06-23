882fc0582d4f7e7d848c39d0847066488309f0a6
davidhenshaw <davidhenshaw2014@gmail.com>
2021-06-23 17:42:43 -0500

    before pledges backend setup

--- client/src/containers/StartupPage.js
+++ client/src/containers/StartupPage.js
@@ -2,10 +2,15 @@ import axios from 'axios';
 import React, { Component, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
+let currencyUS = Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
+
 function StartupPage(props)
 {
     const [startup, setStartup] = useState(null);
     let { id } = useParams();
+
+    //Converts the numbers representing money to have dollar signs, commas, and decimal points
+
 
     useEffect( () => {
             axios.get(`/startups/${id}`)
@@ -32,21 +37,39 @@ function StartupPage(props)
                 <img alt="startup-image" src=""/>
             </div>
             <div className="startup-info">
-                <h3>Funding Goal: {startup.goal}</h3>
-                <h3>Amount Funded:{startup.amount_funded}</h3>
-                <PledgeForm />
+                <h3>Funding Goal: <p>{currencyUS.format(startup.goal)}</p></h3>
+                <h3>Amount Funded: <p>{currencyUS.format(startup.amount_funded)}</p></h3>
+                <PledgeForm user={props.user} id={id}/>
             </div>
         </div>
     );
     
   }
 
-  function PledgeForm(){
+
+  function PledgeForm(props)
+  {
+    const [amount, setAmount] = useState(0);
+
+    function submitPledge(event)
+    {
+        event.preventDefault();
+
+        let pledge = {
+            user_id: props.user.id,
+            startup_id: props.id,
+            amount: amount
+        }
+
+        axios.post(`/pledges`, pledge)
+        .then( console.log )
+    }
+
 
     return(
-        <form>
+        <form onSubmit={(e) => submitPledge(e)}>
             <label>Pledge Amount:</label>
-            <input type="number"></input>
+            <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
             <button>Make a Pledge!</button>
         </form>
     )
