15c7dcd6c76c710b393e89023b0f1cf92f41a9bb
davidhenshaw <davidhenshaw2014@gmail.com>
2021-06-23 19:03:14 -0500

    added pledging validation

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,22 @@
+require 'pry'
 class Pledge < ApplicationRecord
     belongs_to :user
     belongs_to :startup
+
+    validate :user_must_have_sufficient_funds
+
+
+    def user_must_have_sufficient_funds
+        amounts = user.pledges.collect do |pledge|
+            pledge.amount
+        end
+
+        #add the amount we're currently trying to pledge
+        sum = amounts.sum + amount
+
+        binding.pry
+        unless sum < user.funds 
+            errors.add(:amount, "User has insufficient funds to make this pledge!")
+        end
+    end
 end
